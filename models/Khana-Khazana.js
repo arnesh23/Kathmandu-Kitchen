a27f94b9b40b804f50ff09886c273a153daecfbf
Arnesh Regmi <arneshregmi@Arneshs-MacBook-Pro.local>
2018-12-26 16:33:31 -0800

    Khana-Khazana.js file created with calls to the Khana ORM

---
+++ models/Khana-Khazana.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js")
+
+var khana = {
+    selectAll: function(cb){
+        orm.selectAll("Khana",function(res){
+            cb(res);
+        });
+    },
+    insertOne: function(khana,cb){
+        orm.insertOne("Khana", khana, function(res){
+            cb(res);
+        })
+    },
+    updateOne: function(khana,id,cb){
+        orm.updateOne("Khana", khana,id,function(res){
+            cb(res);
+        } 
+    )
+    }
+}
+
+module.exports = khana;
